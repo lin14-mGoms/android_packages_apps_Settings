4fdc3577b7fe599f3d72d2a6ee26c30652869130
mse1969 <mse1969@posteo.de>
2018-09-22 20:46:54 +0200

    Settings: create z_iptables service and iptables block script [2/3]
    Create menu item with checkbox in 'Data usage' to trigger z_iptables
    service for the iptables block script
    
    Change-Id: I0ecc610a8d3bbeb1a16f34952fa21c640f436b2f

--- src/com/android/settings/datausage/DataUsageSummary.java
+++ src/com/android/settings/datausage/DataUsageSummary.java
@@ -143,6 +143,8 @@ public class DataUsageSummary extends DataUsageBase
         if (UserManager.get(getContext()).isAdminUser()) {
             inflater.inflate(R.menu.data_usage, menu);
             menu.findItem(R.id.captive_portal_switch).setChecked(isCaptivePortalDisabled());
+            menu.findItem(R.id.ziptables_block_switch).setChecked(
+                    SystemProperties.getBoolean("persist.privacy.iptab_blk", false));
         }
         super.onCreateOptionsMenu(menu, inflater);
     }
@@ -201,6 +203,14 @@ public class DataUsageSummary extends DataUsageBase
                             .show();
                     mCaptivePortalWarningDialog.setOnDismissListener(this);
                 }
+                return true;
+            }
+            case R.id.ziptables_block_switch: {
+                boolean propValue = SystemProperties.
+                            getBoolean("persist.privacy.iptab_blk", false);
+                propValue = !propValue;
+                SystemProperties.set("persist.privacy.iptab_blk", propValue ? "1" : "0" );
+                item.setChecked(propValue);
                 return true;
             }
         }
