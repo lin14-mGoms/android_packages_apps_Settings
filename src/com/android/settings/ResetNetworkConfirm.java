a92d65919a80fbe43f851aa2e2fa366bee6f8540
mse1969 <mse1969@posteo.de>
2019-04-19 17:22:40 +0200

    Network reset: Also reset iptables block script
    If a user decides to reset all network settings, the custom ip(6)tables
    settings should also be reset.
    
    Change-Id: Idfabaf8d26b2021314133583699920fe2cfab025

--- src/com/android/settings/ResetNetworkConfirm.java
+++ src/com/android/settings/ResetNetworkConfirm.java
@@ -23,6 +23,7 @@ import android.net.ConnectivityManager;
 import android.net.NetworkPolicyManager;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.os.UserManager;
 import android.provider.Settings;
@@ -108,6 +109,8 @@ public class ResetNetworkConfirm extends OptionsMenuFragment {
 
             Settings.Global.putInt(context.getContentResolver(),
                     Settings.Global.CAPTIVE_PORTAL_MODE, 1);
+
+            SystemProperties.set("persist.privacy.iptab_blk", "0");
 
             Toast.makeText(context, R.string.reset_network_complete_toast, Toast.LENGTH_SHORT)
                     .show();
